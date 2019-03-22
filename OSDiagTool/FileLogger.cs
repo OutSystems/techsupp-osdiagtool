c5cea4ab65c84d51bb2859fa39ebaae504944135
fcisco-mc <45656839+fcisco-mc@users.noreply.github.com>
2019-03-22 19:04:44 +0000

    Commit of current last version of OSDiagTool

---
+++ OSDiagTool/FileLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime;
+
+namespace OSDiagTool
+{
+    public class FileLogger
+    {
+        private static string _tempFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "collect_data");
+        private static string _errorDumpFile = Path.Combine(_tempFolderPath, "ConsoleLog.txt");
+
+
+        public static void LogError(string customMessage, string errorMessage)
+        {
+            Console.WriteLine("[ERROR] " + customMessage + ": " + errorMessage);
+            File.AppendAllText(_errorDumpFile, DateTime.Now + "\t" + "[ERROR] \t" + customMessage + "\t" + errorMessage + Environment.NewLine);
+        }
+
+        public static void TraceLog(string traceMessage, bool isTaskFinished = false)
+        {
+            if (isTaskFinished == false)
+            {
+                Console.Write(traceMessage);
+                File.AppendAllText(_errorDumpFile, DateTime.Now + "\t" + traceMessage);
+            }
+            else if (isTaskFinished == true)
+            {
+                Console.WriteLine(traceMessage);
+                File.AppendAllText(_errorDumpFile, traceMessage + Environment.NewLine);
+            }
+
+        }
+
+    }
+}
---
+++ OSDiagTool/README.txt
@@ -0,0 +1,8 @@
+1 - This tool is owned by OutSystems Support;
+2 - If you are reading this, you were probably asked by OutSystems Support to run this executable on the server and send the collected data;
+3 - The tool does not write or modify any machine configurations. It will only read and export this information;
+4 - To run the tool, execute the OSDiagTool.exe;
+5 - Administrator permissions are necessary to run the tool;
+6 - By running the executable, you are accepting the License agreement of Sysinternals Software [1];
+
+[1] - https://docs.microsoft.com/en-us/sysinternals/license-terms 
