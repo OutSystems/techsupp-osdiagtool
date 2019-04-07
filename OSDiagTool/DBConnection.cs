ba2aa7627c6981a189065ca336ac1a2e0762cf88
Francisco Baptista <45656839+fcisco-mc@users.noreply.github.com>
2019-04-07 19:01:57 +0100

    Refactoring of DBConnection.cs
    - Database connection methods separation;

--- OSDiagTool/DBConnection.cs
+++ OSDiagTool/DBConnection.cs
@@ -11,26 +11,45 @@ namespace OSDiagTool
 {
     class DBConnection
     {
-        // SQL Server DB Connection
-        public static string SQLDBConnection(string dataSource, string initialCatalog, string userId, string pwd)
+        // SQL Server DB Connection String
+        public static string SQLConnString(string dataSource, string initialCatalog, string userId, string pwd)
         {
             string connectionStringSQL = String.Format("Data Source={0};Initial Catalog={1};User id={2};Password={3};", dataSource, initialCatalog, userId, pwd);
 
             return connectionStringSQL;
         }
 
-        // SQL Reader
-        public void SQLReader(string dataSource, string initialCatalog, string userId, string pwd, string queryString)
+        // SQL Open Connection
+        public static SqlConnection SQLOpenConnection(string dataSource, string initialCatalog, string userId, string pwd)
         {
-            string connectionString = SQLDBConnection(dataSource, initialCatalog, userId, pwd);
+            string connectionString = SQLConnString(dataSource, initialCatalog, userId, pwd);
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (Exception e)
+            {
+                SQLCloseConnection(connection);
+                Console.WriteLine("Unable to open SQL DB connection" + e.Message);
+                throw e;
+            }
+        }
+
+        // SQL Reader
+        public static void SQLReader(string dataSource, string initialCatalog, string userId, string pwd, string queryString)
+        {
+            SqlConnection connection = SQLOpenConnection(dataSource, initialCatalog, userId, pwd);
+
+            using (connection)
             {
                 SqlCommand command = new SqlCommand(queryString, connection);
 
                 try
                 {
-                    connection.Open();
                     SqlDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
@@ -41,32 +60,61 @@ namespace OSDiagTool
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Unable to retrieve SQL DB information" + e.Message);
+                    Console.WriteLine("Unable to read data from SQL DB: ", e);
+                }
+                finally
+                {
+                    SQLCloseConnection(connection);
                 }
             }
         }
 
+        // SQL Close Connection
+        public static void SQLCloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+        }
 
-        // Oracle DB Connection
-        public static string OracleDBConnection(string host, string port, string serviceName, string userId, string pwd)
+
+        // Oracle DB Connection String
+        public static string OracleConnString(string host, string port, string serviceName, string userId, string pwd)
         {
             string connectionStringORCL = String.Format("SERVER=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT={1}))(CONNECT_DATA=(SERVICE_NAME={2})));uid = {3}; pwd = {4};", host, port, serviceName, userId, pwd);
 
             return connectionStringORCL;
         }
 
-        // Oracle Reader
-        public void OracleReader(string host, string port, string serviceName, string userId, string pwd, string queryString)
+        // Oracle Open Connection
+        public static OracleConnection OracleOpenConnection(string host, string port, string serviceName, string userId, string pwd)
         {
-            string connectionString = OracleDBConnection(host, port, serviceName, userId, pwd);
+            string connectionString = OracleConnString(host, port, serviceName, userId, pwd);
 
-            using (OracleConnection connection = new OracleConnection(connectionString))
+            OracleConnection connection = new OracleConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (Exception e)
+            {
+                OracleCloseConnection(connection);
+                Console.WriteLine("Unable to retrieve Oracle DB Information" + e.Message);
+                throw e;
+            }
+        }
+
+        // Oracle Reader
+        public static void OracleReader(string host, string port, string serviceName, string userId, string pwd, string queryString)
+        {
+            OracleConnection connection = OracleOpenConnection(host, port, serviceName, userId, pwd);
+
+            using (connection)
             {
                 OracleCommand command = new OracleCommand(queryString, connection);
 
                 try
                 {
-                    connection.Open();
                     OracleDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
@@ -77,10 +125,20 @@ namespace OSDiagTool
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Unable to retrieve Oracle DB Information" + e.Message);
+                    Console.WriteLine("Unable to read data from Oracle DB: ", e);
+                }
+                finally
+                {
+                    OracleCloseConnection(connection);
                 }
             }
         }
-      
+
+        // Oracle Close Connection
+        public static void OracleCloseConnection(OracleConnection connection)
+        {
+            connection.Close();
+        }
+
     }
 }
