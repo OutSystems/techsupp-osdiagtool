8c36cca3779578738c4dfd526b37dda71f7ae626
Adriano Ramos <adriano.ramos@outsystems.com>
2021-08-05 20:26:19 +0100

    Added missing library.

--- OSDiagTool/Platform/PlatformDiagnostic.cs
+++ OSDiagTool/Platform/PlatformDiagnostic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
 using Oracle.ManagedDataAccess.Client;
 using OSDiagTool.Platform.ConfigFiles;
 using OSDiagTool.Utils;
