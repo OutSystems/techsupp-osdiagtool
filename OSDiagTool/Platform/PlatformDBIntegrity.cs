f0f1d7d47a6db46cd9f31d43071c13fb6af2c56d
Francisco Baptista <francisco.baptista@outsystems.com>
2024-01-05 15:31:01 +0000

    Integrity check for developers tenant

--- OSDiagTool/Platform/PlatformDBIntegrity.cs
+++ OSDiagTool/Platform/PlatformDBIntegrity.cs
@@ -14,17 +14,21 @@ namespace OSDiagTool.Platform
         public static Dictionary<string, bool> checks = new Dictionary<string, bool>()
             {
                 { "RPM-4012-OK", false },
+                { "Devs_Tenant-OK", false },
             };
 
         public static void RunDBIntegrityCheck(string dbEngine, OSDiagToolConf.ConfModel.strConfModel configurations, string outputDestination,  DBConnector.SQLConnStringModel SQLConnectionString = null,
             DBConnector.OracleConnStringModel OracleConnectionString = null, string adminSchema = null)
         {
-            bool ModulesMappingOK = CheckModulesMappingOK(dbEngine, configurations, outputDestination, "RPM-4012-OK", SQLConnectionString: SQLConnectionString, OracleConnectionString: OracleConnectionString);
-            FileLogger.TraceLog("Check modules mapping OK result: " + ModulesMappingOK);
+            bool modulesMappingOK = CheckModulesMappingOK(dbEngine, configurations, outputDestination, "RPM-4012-OK", SQLConnectionString, OracleConnectionString);
+            FileLogger.TraceLog("Check modules mapping OK result: " + modulesMappingOK);
+
+            bool devsTenantsOK = CheckDevelopersTenantOK(dbEngine, configurations, outputDestination, "Devs_Tenant-OK", SQLConnectionString, OracleConnectionString);
+            FileLogger.TraceLog("Check developers tenant OK result: " + devsTenantsOK);
 
         }
 
-
+        // This method checks if all extensions and espaces have a corresponding entry in the OSSYS_MODULE 
         private static bool CheckModulesMappingOK(string dbEngine, OSDiagToolConf.ConfModel.strConfModel configurations, string outputDestination, string check, DBConnector.SQLConnStringModel SQLConnectionString = null,
             DBConnector.OracleConnStringModel OracleConnectionString = null, string adminSchema = null)
         {
@@ -33,7 +37,6 @@ namespace OSDiagTool.Platform
             List<List<object>> ossys_espace = new List<List<object>>();
             List<int> missingExtensionIds = new List<int>();
             List<int> missingESpaceIds = new List<int>();
-
 
             IDatabaseConnection connection = DatabaseConnectionFactory.GetDatabaseConnection(dbEngine, SQLConnectionString, OracleConnectionString);
 
@@ -102,6 +105,57 @@ namespace OSDiagTool.Platform
                 writer.WriteLine(string.Format("== A Platform integrty issue was found - please open a support case to follow up and provide the output of OSDiagTool ==" + Environment.NewLine));
                 writer.WriteLine(string.Format("* Missing eSpace Ids in OSSYS_MODULE table: {0}", String.Join(", ", missingESpaceIds)));
                 writer.WriteLine(string.Format("* Missing extension Ids in OSSYS_MODULE table: {0}", String.Join(", ", missingExtensionIds)));
+            }
+
+            return checks[check] = false;
+        }
+
+        // This method cross references the User_ID of OSSYS_USER_DEVELOPER with the User_Id of OSSYS_USER and checks if the users have Service Center tenant (1)
+        private static bool CheckDevelopersTenantOK(string dbEngine, OSDiagToolConf.ConfModel.strConfModel configurations, string outputDestination, string check, DBConnector.SQLConnStringModel SQLConnectionString = null,
+            DBConnector.OracleConnStringModel OracleConnectionString = null, string adminSchema = null)
+        {
+            List<List<object>> ossys_user = new List<List<object>>();
+            List<List<object>> ossys_user_developer = new List<List<object>>();
+            Dictionary<int, int> devsWithWrongTenant = new Dictionary<int,int>();
+
+            IDatabaseConnection connection = DatabaseConnectionFactory.GetDatabaseConnection(dbEngine, SQLConnectionString, OracleConnectionString);
+
+            using (connection)
+            {
+                IDatabaseCommand commandExecutor = DatabaseCommandFactory.GetCommandExecutor(dbEngine, connection);
+                ossys_user = commandExecutor.ReadData("SELECT ID, TENANT_ID FROM OSSYS_USER WHERE IS_ACTIVE=1;", configurations, connection).Select(row => row.ToList()).ToList();
+                ossys_user_developer = commandExecutor.ReadData("SELECT DISTINCT USER_ID FROM OSSYS_USER_DEVELOPER;", configurations, connection).Select(row => row.ToList()).ToList();
+            }
+
+            for (int i = 0; i < ossys_user_developer.Count; i++)
+            {
+                object user_devId = ossys_user_developer[i][0];
+                bool user_devId_OK = false;
+
+                object userId = ossys_user[(int)user_devId-1][0];
+                object tenantId = ossys_user[(int)user_devId-1][1];
+
+                if (tenantId.Equals(1))
+                {
+                    user_devId_OK = true;
+                }
+                else
+                {
+                    devsWithWrongTenant.Add((int)user_devId, (int)tenantId);
+                }
+            }
+
+            if (devsWithWrongTenant.Count.Equals(0))
+            {
+                return checks[check] = true;
+            }
+
+            var lines = devsWithWrongTenant.Select(kvp => kvp.Key + ": " + kvp.Value.ToString());
+
+            using (TextWriter writer = new StreamWriter(File.Create(Path.Combine(outputDestination, check + ".txt"))))
+            {
+                writer.WriteLine(string.Format("== A Platform integrty issue was found - please open a support case to follow up and provide the output of OSDiagTool ==" + Environment.NewLine));
+                writer.WriteLine(string.Format("* User Id of developers with wrong tenants: " + Environment.NewLine + String.Join(Environment.NewLine, devsWithWrongTenant.Select(kv => $"- User ID {kv.Key} with tenant {kv.Value}"))));
             }
 
             return checks[check] = false;
